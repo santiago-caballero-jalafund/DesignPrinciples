5ad0847d80f9a18474c71768bc0b931f90c2f27c
0sanisimo0 <santicm95386b@gmail.com>
2024-07-24 00:24:48 -0400

    feat: implement KISS and YAGNI; improve User and UserManager

--- LeastAstonishment/User.cs
+++ LeastAstonishment/User.cs
@@ -7,13 +7,6 @@ public class User
   public string Email { get; set; }
   
   public string Password { get; set; }
-  
-  public string Role { get; set; }
-
-  public User()
-  {
-    //...
-  }
 
   public void UpdatePassword(string newPassword)
   {
--- LeastAstonishment/UserManager.cs
+++ LeastAstonishment/UserManager.cs
@@ -1,50 +1,50 @@
 ﻿namespace LeastAstonishment;
 
 public class UserManager
-{
-  private readonly List<User> _users;
-
-  public UserManager()
-  {
-    _users = [];
-  }
-
-  public void AddUser(string username, string email, string password)
-  {
-    _users.Add(new User { Username = username, Email = email, Password = password });
-  }
-
-  public void UpdateUserPassword(string username, string newPassword)
-  {
-    var user = _users.FirstOrDefault(u => u.Username == username);
-
-    if (user != null)
     {
-      user.UpdatePassword(newPassword);
-    }
-    else
-    {
-      Console.WriteLine($"User with username {username} not found.");
-    }
-  }
+    private readonly List<User> _users;
 
-  public void SetUserRole(string username, string role)
-  {
-    var user = _users.FirstOrDefault(u => u.Username == username);
-    
-    if (user != null)
+    public UserManager()
     {
-      user.Role = role;
+        _users = new List<User>();
     }
-  }
 
-  public void DisplayUserDetails(string username)
-  {
-    var user = _users.FirstOrDefault(u => u.Username == username);
-    
-    if (user != null)
+    private bool DoesUserExists(User user) 
     {
-      Console.WriteLine($"Username: {user.Username}, Email: {user.Email}, Role: {user.Role}");
+      return user != null;
     }
-  }
+
+    public void AddUser(string username, string email, string password)
+    {
+        _users.Add(new User { Username = username, Email = email, Password = password });
+    }
+
+    public void UpdateUserPassword(string username, string newPassword)
+    {
+        var user = _users.FirstOrDefault(u => u.Username == username);
+
+        if (DoesUserExists(user))
+        {
+            user.UpdatePassword(newPassword);
+        }
+        else
+        {
+            DisplayAlertMessage("User with username {username} not found.");
+        }
+    }
+
+    public void DisplayUserDetails(string username)
+    {
+        var user = _users.FirstOrDefault(u => u.Username == username);
+
+        if (DoesUserExists(user))
+        {
+            DisplayAlertMessage("Username: {user.Username}, Email: {user.Email}");
+        }
+    }
+
+    private void DisplayAlertMessage(string message)
+    {
+      Console.WriteLine('$'+message);
+    }
 }
